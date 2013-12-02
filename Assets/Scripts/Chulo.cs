9bbbcf75efb97fc40c0e74e9ea6db2b087c4e10a
Jose Antonio <josementi@gmail.com>
2013-12-02 12:36:46 +0100

    añadidas varias animaciones y el background

---
+++ Assets/Scripts/Chulo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Chulo : LWFObject {
+
+	// Use this for initialization
+	void Start () {
+        string dir = System.IO.Path.GetDirectoryName(lwfName);
+        if (dir.Length > 0)
+            dir += "/";
+
+        if (Application.isEditor)
+            UseDrawMeshRenderer();
+
+        Load(lwfName, dir);
+        Scale(0.33f, 0.33f);
+	}
+	
+	
+}
---
+++ Assets/Scripts/Girlfriend.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Girlfriend : LWFObject {
+
+    void Start()
+    {
+        string dir = System.IO.Path.GetDirectoryName(lwfName);
+        if (dir.Length > 0)
+            dir += "/";
+
+        if (Application.isEditor)
+            UseDrawMeshRenderer();
+
+        Load(lwfName, dir);
+        Scale(0.5f, 0.5f);
+    }
+
+}
