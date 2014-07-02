6a8c8469d930fdf646b9b6de6c51e912d9f7c00f
Jose Antonio <josementi@gmail.com>
2014-07-02 16:43:28 +0200

    Grid, new background

---
+++ Assets/Scrips/GridController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Puppycode.ScreenUtility;
+
+public class GridController : MonoBehaviour {
+
+    GameObject[,] enemies;
+	// Use this for initialization
+	void Start () {
+        enemies = new GameObject[5, 8];
+        GameObject go = (GameObject)Resources.Load("test");
+        for(int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+               GameObject g =  (GameObject)Instantiate(go, Camera.main.ScreenToWorldPoint(new Vector3(PuppyScreen.Width(19f) + PuppyScreen.Width(10f) * j, PuppyScreen.Height(23.5f) + PuppyScreen.Height(14.58f) * i, +3)), Quaternion.identity);
+               g.GetComponent<Tile>().info = i + "," + j;
+            }
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Ended)
+            {
+                ;
+            }
+        }
+	}
+}
--- Assets/Scrips/ScenarioController.cs
+++ Assets/Scrips/ScenarioController.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
+/// <summary>
+/// Loads the lwf of the scenario depending on the aspect of the Screen
+/// </summary>
 [ExecuteInEditMode]
 public class ScenarioController : LWFObject{
 
 	// Use this for initialization
 	void Start () {
-		string dir = System.IO.Path.GetDirectoryName(lwfName);
+        string scenario = "Envs/env_" + PlayerPrefs.GetString("scenario", "01") + "_" + Camera.main.aspect.ToString().Replace(".", "").Substring(0, 2) + ".swf/env_" + PlayerPrefs.GetString("scenario", "01") + "_" + Camera.main.aspect.ToString().Replace(".", "").Substring(0, 2);
+        string dir = System.IO.Path.GetDirectoryName(scenario);
 		if (dir.Length > 0)
 			dir += "/";
-		//Scale (0.5f, 0.5f);
-		Load(lwfName, dir);
-		print(this.lwf.height);
-		print(this.lwf.width);
+		Load(scenario, dir);
+        Camera.main.orthographicSize = this.lwf.height / 2;
+		this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
+		this.transform.position = new Vector3(this.transform.position.x - this.lwf.width, this.transform.position.y, 0);
 	}
 }
